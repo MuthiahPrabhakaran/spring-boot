9d057b46bcc08391c9febbcbe07098c22481215d
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2020-05-20 08:14:49 +0530

    Query DSL - Like added

--- spring-boot-jpa/ps-guitar-db/src/main/java/com/guitar/db/repository/LocationRepository.java
+++ spring-boot-jpa/ps-guitar-db/src/main/java/com/guitar/db/repository/LocationRepository.java
@@ -4,6 +4,10 @@ package com.guitar.db.repository;
 import com.guitar.db.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    List<Location> findByStateLike(String stateName);
 
 }
--- spring-boot-jpa/ps-guitar-db/src/test/java/com/guitar/db/repository/LocationRepositoryTest.java
+++ spring-boot-jpa/ps-guitar-db/src/test/java/com/guitar/db/repository/LocationRepositoryTest.java
@@ -5,14 +5,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:application.properties")
 public class LocationRepositoryTest {
 
 	@Autowired
@@ -22,5 +26,38 @@ public class LocationRepositoryTest {
 	public void testFindAll() {
 		List<Location> locations = locationRepository.findAll();
 		assertNotNull(locations);
+	}
+
+	@Test
+	public void testSaveAndGetAndDelete(){
+		Location location = new Location();
+		location.setCountry("Canada");
+		location.setState("British Columbia");
+		location = locationRepository.save(location);
+
+		Location otherLocation = locationRepository.findById(location.getId()).get();
+		assertEquals("Canada", otherLocation.getCountry());
+		assertEquals("British Columbia", otherLocation.getState());
+
+		//delete BC location now
+		locationRepository.delete(otherLocation);
+	}
+
+	@Test
+	public void testFindWithLike(){
+		List<Location> locations = locationRepository.findByStateLike("New%");
+		assertEquals(4, locations.size());
+	}
+
+	@Test
+	@Transactional  //note this is needed because we will get a lazy load exception unless we are in a tx
+	public void testFindWithChildren() throws Exception {
+		Location arizona = locationRepository.findById(3L).get();
+		assertEquals("United States", arizona.getCountry());
+		assertEquals("Arizona", arizona.getState());
+
+		assertEquals(1, arizona.getManufacturers().size());
+
+		assertEquals("Fender Musical Instruments Corporation", arizona.getManufacturers().get(0).getName());
 	}
 }
