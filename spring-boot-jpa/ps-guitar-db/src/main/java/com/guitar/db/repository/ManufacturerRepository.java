79b593ee293d1283e60d62b98e9732eb07b7a5f0
Muthiah Prabhakaran <muthiahprabhakaran@gmail.com>
2020-05-21 08:22:26 +0530

    Date, less than, greater than

--- spring-boot-jpa/ps-guitar-db/src/main/java/com/guitar/db/repository/ManufacturerRepository.java
+++ spring-boot-jpa/ps-guitar-db/src/main/java/com/guitar/db/repository/ManufacturerRepository.java
@@ -3,5 +3,10 @@ package com.guitar.db.repository;
 import com.guitar.db.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ManufacturerRepository extends JpaRepository<Manufacturer, Long> {
+
+    List<Manufacturer> findByFoundedDateBefore(Date foundedDate);
 }
--- spring-boot-jpa/ps-guitar-db/src/main/java/com/guitar/db/repository/ModelRepository.java
+++ spring-boot-jpa/ps-guitar-db/src/main/java/com/guitar/db/repository/ModelRepository.java
@@ -3,5 +3,10 @@ package com.guitar.db.repository;
 import com.guitar.db.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ModelRepository extends JpaRepository<Model, Long> {
+
+    List<Model> findByPriceGreaterThanEqualAndLessThanEqual(BigDecimal low, BigDecimal High);
 }
